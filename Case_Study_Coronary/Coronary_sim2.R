15bd9307be07e5f45be45d678d4347685452f6be
Yicheng Shen <shenyicheng1109@gmail.com>
2023-06-12 19:44:22 -0400

    add one more study in coronary

--- Case_Study_Coronary/Coronary_sim2.R
+++ Case_Study_Coronary/Coronary_sim2.R
@@ -453,3 +453,182 @@ colnames(result_rate_bleeding) <- c("Power AB","Power AC","Power AD",
                                     "Rank A last")
 
 save(result_rate_bleeding, file = "result_rate_bleeding_CD.RData")
+
+
+
+
+# add BC
+Bleed_data_BC = rbind(Bleed_data, c(5, "B", round(mean(Bleed_data$sampleSize)), round(mean(Bleed_data$sampleSize) * pi_b)) )
+Bleed_data_BC = rbind(Bleed_data_BC, c(5, "C", round(mean(Bleed_data$sampleSize)), round(mean(Bleed_data$sampleSize) * pi_c)) )
+Bleed_data_BC$sampleSize = as.numeric(Bleed_data_BC$sampleSize)
+Bleed_data_BC$responders = as.numeric(Bleed_data_BC$responders)
+n_study = n_distinct(Bleed_data_BC$study)
+
+result_bleeding = foreach (i = 1:S, .combine = "+", .errorhandling='remove') %dopar% {
+  
+  y_ik = c()
+  for (i in 1:n_study){
+    study_data = Bleed_data_BC[Bleed_data_BC$study==i,]
+    
+    alpha_iB = rnorm(n=1, mean=logit(pi_a), sd=0.1)
+    for (k in 1:nrow(study_data)){
+      if (k == 1) {
+        logit_p_ik = alpha_iB 
+        p_ik = inverse_logit(logit_p_ik)
+        y_ik = c(y_ik, rbinom(n = 1, size = study_data$sampleSize[k], prob = p_ik))
+      } else {
+        d_k = Log_OR_dat[which(trt == study_data$treatment[k])]
+        delta_iBk = rnorm(n=1, mean=d_k, sd=tau)
+        logit_p_ik = alpha_iB + delta_iBk
+        p_ik = inverse_logit(logit_p_ik)
+        y_ik = c(y_ik, rbinom(n = 1, size = study_data$sampleSize[k], prob = p_ik))
+      }
+    }
+  }
+  
+  sim_dat = Bleed_data_BC
+  sim_dat$responders = y_ik
+  
+  network <- gemtc::mtc.network(data.ab=sim_dat, treatments=trts)
+  cons.model <- gemtc::mtc.model(network, type="consistency", likelihood="binom", link="logit", linearModel="random")
+  cons.out <-gemtc::mtc.run(cons.model, n.adapt=500, n.iter=2000, thin=1)
+  
+  prob <- gemtc::rank.probability(cons.out, preferredDirection = -1)
+  prob <- round(prob, digits=3)
+  sucra <- gemtc::sucra(prob)
+  rank_order = rownames(as.matrix(sort(sucra, decreasing = T)))
+  A_last = as.numeric(rank_order[4] == "A")
+  
+  res = summary(gemtc::relative.effect(cons.out,"A", c("B","C","D")))
+  
+  AB_lower_95 = res$summaries$quantiles[1,1]
+  AB_upper_95 = res$summaries$quantiles[1,5]
+  AB_reject = 1-as.numeric(0 >= AB_lower_95 & 0 <= AB_upper_95)
+  
+  AC_lower_95 = res$summaries$quantiles[2,1]
+  AC_upper_95 = res$summaries$quantiles[2,5]
+  AC_reject = 1-as.numeric(0 >= AC_lower_95 & 0 <= AC_upper_95)
+  
+  AD_lower_95 = res$summaries$quantiles[3,1]
+  AD_upper_95 = res$summaries$quantiles[3,5]
+  AD_reject = 1-as.numeric(0 >= AD_lower_95 & 0 <= AD_upper_95)
+  
+  res = summary(gemtc::relative.effect(cons.out,c("B","B","C"), c("C","D","D")))
+  
+  BC_lower_95 = res$summaries$quantiles[1,1]
+  BC_upper_95 = res$summaries$quantiles[1,5]
+  BC_reject = 1-as.numeric(0 >= BC_lower_95 & 0 <= BC_upper_95)
+  
+  BD_lower_95 = res$summaries$quantiles[2,1]
+  BD_upper_95 = res$summaries$quantiles[2,5]
+  BD_reject = 1-as.numeric(0 >= BD_lower_95 & 0 <= BD_upper_95)
+  
+  CD_lower_95 = res$summaries$quantiles[3,1]
+  CD_upper_95 = res$summaries$quantiles[3,5]
+  CD_reject = 1-as.numeric(0 >= CD_lower_95 & 0 <= CD_upper_95)
+  
+  reject_correct = c(AB_reject, AC_reject, AD_reject)
+  reject_incorrect = c(BC_reject, BD_reject, CD_reject)
+  
+  return(c(reject_correct, reject_incorrect, A_last))
+}
+
+# first three powers / next three Type I errors / last one A being ranked last
+result_rate_bleeding = result_bleeding/S
+
+result_rate_bleeding = matrix(result_rate_bleeding, nrow = 1)
+colnames(result_rate_bleeding) <- c("Power AB","Power AC","Power AD",
+                                    "Type I err BC","Type I err BD","Type I err CD",
+                                    "Rank A last")
+
+save(result_rate_bleeding, file = "result_rate_bleeding_BC.RData")
+
+
+
+
+
+# add BC
+Bleed_data_BD = rbind(Bleed_data, c(5, "B", round(mean(Bleed_data$sampleSize)), round(mean(Bleed_data$sampleSize) * pi_b)) )
+Bleed_data_BD = rbind(Bleed_data_BD, c(5, "D", round(mean(Bleed_data$sampleSize)), round(mean(Bleed_data$sampleSize) * pi_d)) )
+Bleed_data_BD$sampleSize = as.numeric(Bleed_data_BD$sampleSize)
+Bleed_data_BD$responders = as.numeric(Bleed_data_BD$responders)
+n_study = n_distinct(Bleed_data_BD$study)
+
+result_bleeding = foreach (i = 1:S, .combine = "+", .errorhandling='remove') %dopar% {
+  
+  y_ik = c()
+  for (i in 1:n_study){
+    study_data = Bleed_data_BD[Bleed_data_BD$study==i,]
+    
+    alpha_iB = rnorm(n=1, mean=logit(pi_a), sd=0.1)
+    for (k in 1:nrow(study_data)){
+      if (k == 1) {
+        logit_p_ik = alpha_iB 
+        p_ik = inverse_logit(logit_p_ik)
+        y_ik = c(y_ik, rbinom(n = 1, size = study_data$sampleSize[k], prob = p_ik))
+      } else {
+        d_k = Log_OR_dat[which(trt == study_data$treatment[k])]
+        delta_iBk = rnorm(n=1, mean=d_k, sd=tau)
+        logit_p_ik = alpha_iB + delta_iBk
+        p_ik = inverse_logit(logit_p_ik)
+        y_ik = c(y_ik, rbinom(n = 1, size = study_data$sampleSize[k], prob = p_ik))
+      }
+    }
+  }
+  
+  sim_dat = Bleed_data_BD
+  sim_dat$responders = y_ik
+  
+  network <- gemtc::mtc.network(data.ab=sim_dat, treatments=trts)
+  cons.model <- gemtc::mtc.model(network, type="consistency", likelihood="binom", link="logit", linearModel="random")
+  cons.out <-gemtc::mtc.run(cons.model, n.adapt=500, n.iter=2000, thin=1)
+  
+  prob <- gemtc::rank.probability(cons.out, preferredDirection = -1)
+  prob <- round(prob, digits=3)
+  sucra <- gemtc::sucra(prob)
+  rank_order = rownames(as.matrix(sort(sucra, decreasing = T)))
+  A_last = as.numeric(rank_order[4] == "A")
+  
+  res = summary(gemtc::relative.effect(cons.out,"A", c("B","C","D")))
+  
+  AB_lower_95 = res$summaries$quantiles[1,1]
+  AB_upper_95 = res$summaries$quantiles[1,5]
+  AB_reject = 1-as.numeric(0 >= AB_lower_95 & 0 <= AB_upper_95)
+  
+  AC_lower_95 = res$summaries$quantiles[2,1]
+  AC_upper_95 = res$summaries$quantiles[2,5]
+  AC_reject = 1-as.numeric(0 >= AC_lower_95 & 0 <= AC_upper_95)
+  
+  AD_lower_95 = res$summaries$quantiles[3,1]
+  AD_upper_95 = res$summaries$quantiles[3,5]
+  AD_reject = 1-as.numeric(0 >= AD_lower_95 & 0 <= AD_upper_95)
+  
+  res = summary(gemtc::relative.effect(cons.out,c("B","B","C"), c("C","D","D")))
+  
+  BC_lower_95 = res$summaries$quantiles[1,1]
+  BC_upper_95 = res$summaries$quantiles[1,5]
+  BC_reject = 1-as.numeric(0 >= BC_lower_95 & 0 <= BC_upper_95)
+  
+  BD_lower_95 = res$summaries$quantiles[2,1]
+  BD_upper_95 = res$summaries$quantiles[2,5]
+  BD_reject = 1-as.numeric(0 >= BD_lower_95 & 0 <= BD_upper_95)
+  
+  CD_lower_95 = res$summaries$quantiles[3,1]
+  CD_upper_95 = res$summaries$quantiles[3,5]
+  CD_reject = 1-as.numeric(0 >= CD_lower_95 & 0 <= CD_upper_95)
+  
+  reject_correct = c(AB_reject, AC_reject, AD_reject)
+  reject_incorrect = c(BC_reject, BD_reject, CD_reject)
+  
+  return(c(reject_correct, reject_incorrect, A_last))
+}
+
+# first three powers / next three Type I errors / last one A being ranked last
+result_rate_bleeding = result_bleeding/S
+
+result_rate_bleeding = matrix(result_rate_bleeding, nrow = 1)
+colnames(result_rate_bleeding) <- c("Power AB","Power AC","Power AD",
+                                    "Type I err BC","Type I err BD","Type I err CD",
+                                    "Rank A last")
+
+save(result_rate_bleeding, file = "result_rate_bleeding_BD.RData")
