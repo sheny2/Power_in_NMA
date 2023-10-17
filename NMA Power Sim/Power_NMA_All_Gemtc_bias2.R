10d00df71f259b87916201ff6c4c4a4f5c8fbf73
Yicheng Shen <shenyicheng1109@gmail.com>
2023-10-17 15:39:11 -0400

    bias update

--- NMA Power Sim/Power_NMA_All_Gemtc_bias2.R
+++ NMA Power Sim/Power_NMA_All_Gemtc_bias2.R
@@ -47,22 +47,22 @@ registerDoParallel(cl)
 
 # Define a function to be applied in parallel
 power_mutate <- function(df) {
-  df %>% mutate(power = power.sim.NMA3(k_ab, k_ac, k_bc = 0, pi_a, OR_ab, OR_ac, tau))
+  df %>% mutate(power = power.sim.NMA4(N_sim, k_ab, k_ac, k_bc = 0, pi_a, OR_ab, OR_ac, tau))
 }
 
 # Apply the function in parallel using foreach
-df_indirect_bias <- expand_grid(S = 1:N_sim, pi_a, OR_ab, OR_ac, tau, k_ab) %>% 
+df_indirect_bias <- expand_grid(pi_a, OR_ab, OR_ac, tau, k_ab) %>% 
   mutate(OR_bc = round(exp(log(OR_ac) - log(OR_ab)), 2)) %>%
   mutate(pi_b = pi_a * OR_ab / (1 - pi_a + pi_a * OR_ab )) %>% 
   mutate(pi_c = pi_a * OR_ac / (1 - pi_a + pi_a * OR_ac )) %>% 
   mutate(k_ac = k_ab) %>% 
-  group_by(S, k_ab, k_ac, pi_a, OR_ab, OR_ac, tau) %>%
-  do(power_mutate(.))%>% separate(power, c("power", "rank_correct", "point_est", "point_true"), " ", convert = TRUE)
+  group_by(k_ab, k_ac, pi_a, OR_ab, OR_ac, tau) %>%
+  do(power_mutate(.))%>% separate(power, c("power", "rank_correct", "avg_bias"), " ", convert = TRUE)
 
 # Stop the parallel backend
 stopCluster(cl)
 
-save(df_indirect_bias, file = "df_indirect_BNMA_bias.RData")
+save(df_indirect_bias, file = "df_indirect_BNMA_bias2.RData")
 
 
 
@@ -79,21 +79,21 @@ registerDoParallel(cl)
 
 # Define a function to be applied in parallel
 power_mutate <- function(df) {
-  df %>%  mutate(power = power.sim.NMA3(k_ab = 0, k_ac = 0, k_bc, pi_a, OR_ab, OR_ac, tau))
+  df %>%  mutate(power = power.sim.NMA4(N_sim, k_ab = 0, k_ac = 0, k_bc, pi_a, OR_ab, OR_ac, tau))
 }
 
 # Apply the function in parallel using foreach
-df_direct_bias <- expand_grid(S = 1:N_sim, k_bc, pi_a, OR_ab, OR_ac, tau) %>% 
+df_direct_bias <- expand_grid( k_bc, pi_a, OR_ab, OR_ac, tau) %>% 
   mutate(OR_bc = round(exp(log(OR_ac) - log(OR_ab)), 2)) %>% 
   mutate(pi_b = pi_a * OR_ab / (1 - pi_a + pi_a * OR_ab )) %>% 
   mutate(pi_c = pi_a * OR_ac / (1 - pi_a + pi_a * OR_ac )) %>% 
-  group_by(S, k_bc, pi_a, OR_ab, OR_ac, tau) %>% 
-  do(power_mutate(.))%>% separate(power, c("power", "rank_correct", "point_est", "point_true"), " ", convert = TRUE)
+  group_by(k_bc, pi_a, OR_ab, OR_ac, tau) %>% 
+  do(power_mutate(.))%>% separate(power, c("power", "rank_correct", "avg_bias"), " ", convert = TRUE)
 
 # Stop the parallel backend
 stopCluster(cl)
 
-save(df_direct_bias, file = "df_direct_BNMA_bias.RData")
+save(df_direct_bias, file = "df_direct_BNMA_bias2.RData")
 
 
 
@@ -110,20 +110,20 @@ registerDoParallel(cl)
 
 # Define a function to be applied in parallel
 power_mutate <- function(df) {
-  df %>%  mutate(power = power.sim.NMA3(k_ab, k_ac, k_bc, pi_a, OR_ab, OR_ac, tau))
+  df %>%  mutate(power = power.sim.NMA4(N_sim, k_ab, k_ac, k_bc, pi_a, OR_ab, OR_ac, tau))
 }
 
 # Apply the function in parallel using foreach
-df_BNMA_bias <- expand_grid(S = 1:N_sim, pi_a, OR_ab, OR_ac, tau, k_ab, DR_INDR) %>% 
+df_BNMA_bias <- expand_grid(pi_a, OR_ab, OR_ac, tau, k_ab, DR_INDR) %>% 
   mutate(k_ac = k_ab, k_bc = k_ab / DR_INDR) %>% 
   select(-DR_INDR)%>% 
   mutate(OR_bc = round(exp(log(OR_ac) - log(OR_ab)), 2)) %>% 
   mutate(pi_b = pi_a * OR_ab / (1 - pi_a + pi_a * OR_ab )) %>% 
   mutate(pi_c = pi_a * OR_ac / (1 - pi_a + pi_a * OR_ac )) %>% 
-  group_by(S, k_ab, k_bc, pi_a, OR_ab, OR_ac, tau) %>% 
-  do(power_mutate(.)) %>% separate(power, c("power", "rank_correct", "point_est", "point_true"), " ", convert = TRUE)
+  group_by(k_ab, k_bc, pi_a, OR_ab, OR_ac, tau) %>% 
+  do(power_mutate(.)) %>% separate(power, c("power", "rank_correct", "avg_bias"), " ", convert = TRUE)
 
 # Stop the parallel backend
 stopCluster(cl)
 
-save(df_BNMA_bias, file = "df_overall_BNMA_bias.RData")
+save(df_BNMA_bias, file = "df_overall_BNMA_bias2.RData")
