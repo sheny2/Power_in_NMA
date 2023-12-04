def22b9bad635c9337c5a46732d264a015388860
shenyicheng1109@gmail.com <shenyicheng1109@gmail.com>
2023-12-04 10:55:57 -0500

    update

--- Arm-based/diabetes_AB.R
+++ Arm-based/diabetes_AB.R
@@ -76,71 +76,6 @@ result_diabetes_all = foreach (i = 1:S, .combine = "+", .errorhandling='remove')
   library(tidyverse)
   library(gemtc)
   
-  # Log_OR_dat = c(log(1), estimates$summaries$statistics[,1][1:5])
-  # tau = tau_est
-  # 
-  # y_ik = c()
-  # for (i in 1:n_study){
-  #   study_data = diabetes_ab[diabetes_ab$study==i,]
-  #   
-  #   alpha_iB = rnorm(n=1, mean=logit(pi_a), sd=0.1)
-  #   if (study_data$treatment[1] == "1") {
-  #     for (k in 1:nrow(study_data)){
-  #       if (k == 1) {
-  #         logit_p_ik = alpha_iB 
-  #         p_ik = inverse_logit(logit_p_ik)
-  #         y_ik = c(y_ik, rbinom(n = 1, size = study_data$sampleSize[k], prob = p_ik))
-  #       } else {
-  #         d_k = Log_OR_dat[which(trt == study_data$treatment[k])]
-  #         delta_iBk = rnorm(n=1, mean=d_k, sd=tau)
-  #         logit_p_ik = alpha_iB + delta_iBk
-  #         p_ik = inverse_logit(logit_p_ik)
-  #         y_ik = c(y_ik, rbinom(n = 1, size = study_data$sampleSize[k], prob = p_ik))
-  #       }
-  #     }
-  #   } else {for (k in 1:nrow(study_data)){
-  #     d_k = Log_OR_dat[which(trt == study_data$treatment[k])]
-  #     delta_iBk = rnorm(n=1, mean=d_k, sd=tau)
-  #     logit_p_ik = alpha_iB + delta_iBk
-  #     p_ik = inverse_logit(logit_p_ik)
-  #     y_ik = c(y_ik, rbinom(n = 1, size = study_data$sampleSize[k], prob = p_ik))
-  #   }
-  #   }
-  # }
-  # 
-  # sim_dat = diabetes_ab
-  # sim_dat$responders = y_ik
-  
-  # network <- gemtc::mtc.network(data.ab=sim_dat)
-  # cons.model <- gemtc::mtc.model(network, type="consistency", likelihood="binom", link="logit", linearModel="random",
-  #                                hy.prior =  mtc.hy.prior(type="std.dev", distr="dunif", 0.01, 10),
-  #                                re.prior.sd = 10)
-  # cons.out <-gemtc::mtc.run(cons.model, n.iter=5000, n.adapt = 2000, thin=1)
-  # 
-  # # prob <- gemtc::rank.probability(cons.out, preferredDirection = 1)
-  # # prob <- round(prob, digits=3)
-  # # sucra <- gemtc::sucra(prob)
-  # # rank_order = rownames(as.matrix(sort(sucra, decreasing = T)))
-  # # A_last = as.numeric(rank_order[4] == "A")
-  # # Correct_order = as.numeric(identical(rank_order,c("D","C","B", "A")))
-  # 
-  # res = summary(gemtc::relative.effect(cons.out,"1", c("2","3","4","5","6")))
-  # 
-  # AB_lower_95 = res$summaries$quantiles[1,1]
-  # AB_upper_95 = res$summaries$quantiles[1,5]
-  # AB_reject = 1-as.numeric(0 >= AB_lower_95 & 0 <= AB_upper_95)
-  # 
-  # AC_lower_95 = res$summaries$quantiles[2,1]
-  # AC_upper_95 = res$summaries$quantiles[2,5]
-  # AC_reject = 1-as.numeric(0 >= AC_lower_95 & 0 <= AC_upper_95)
-  # 
-  # AD_lower_95 = res$summaries$quantiles[3,1]
-  # AD_upper_95 = res$summaries$quantiles[3,5]
-  # AD_reject = 1-as.numeric(0 >= AD_lower_95 & 0 <= AD_upper_95)
-  # 
-  # reject_correct_gemtc = c(AB_reject, AC_reject, AD_reject)
-  
-  
   ### Data pre for jags, AB model first
   Log_OR_dat = c(0, -0.23834311, 0.02570665, -0.13496070, -0.30540779, -0.49401317)
   tau_AB = c(0.568964683, 0.767673995, 0.630069285, 0.660131261, 0.685801466, 0.825769537)
@@ -300,9 +235,9 @@ result_diabetes_all = foreach (i = 1:S, .combine = "+", .errorhandling='remove')
   study<-unique(sim_dat$study)
   
   for (i in 1:NS){
-    n.obs[i,1:Narm[i]] <- diabetes_ab$sampleSize[diabetes_ab$study==study[i]]
-    n.eve[i,1:Narm[i]] <- diabetes_ab$responders[diabetes_ab$study==study[i]]
-    dr[i,1:Narm[i]] <- match(diabetes_ab$treatment[diabetes_ab$study==study[i]],drug_list)
+    n.obs[i,1:Narm[i]] <- sim_dat$sampleSize[sim_dat$study==study[i]]
+    n.eve[i,1:Narm[i]] <- sim_dat$responders[sim_dat$study==study[i]]
+    dr[i,1:Narm[i]] <- match(sim_dat$treatment[sim_dat$study==study[i]],drug_list)
   }
   
   
@@ -351,94 +286,7 @@ result_diabetes_all = foreach (i = 1:S, .combine = "+", .errorhandling='remove')
   reject_correct_LA = c(AB_reject, AC_reject, AD_reject, AE_reject, AF_reject)
   
   
-  
-  # # CB model
-  # Log_OR_dat = log(c(1, 1.423690, 2.163300, 2.573106))
-  # tau_CB = c(0.4811976, 0.8334492, 0.7683358, 0.9263793)
-  # 
-  # y_ik = c()
-  # for (i in 1:n_study){
-  #   study_data = smokingcessation_ab[smokingcessation_ab$study==i,]
-  #   
-  #   alpha_iB = rnorm(n=1, mean=logit(pi_a), sd=0.1)
-  #   if (study_data$treatment[1] == "A") {
-  #     for (k in 1:nrow(study_data)){
-  #       if (k == 1) {
-  #         logit_p_ik = alpha_iB 
-  #         p_ik = inverse_logit(logit_p_ik)
-  #         y_ik = c(y_ik, rbinom(n = 1, size = study_data$sampleSize[k], prob = p_ik))
-  #       } else {
-  #         d_k = Log_OR_dat[which(trt == study_data$treatment[k])]
-  #         tau = tau_CB[which(trt == study_data$treatment[k])]
-  #         delta_iBk = rnorm(n=1, mean=d_k, sd=tau)
-  #         logit_p_ik = alpha_iB + delta_iBk
-  #         p_ik = inverse_logit(logit_p_ik)
-  #         y_ik = c(y_ik, rbinom(n = 1, size = study_data$sampleSize[k], prob = p_ik))
-  #       }
-  #     }
-  #   } else {for (k in 1:nrow(study_data)){
-  #     d_k = Log_OR_dat[which(trt == study_data$treatment[k])]
-  #     tau = tau_CB[which(trt == study_data$treatment[k])]
-  #     delta_iBk = rnorm(n=1, mean=d_k, sd=tau)
-  #     logit_p_ik = alpha_iB + delta_iBk
-  #     p_ik = inverse_logit(logit_p_ik)
-  #     y_ik = c(y_ik, rbinom(n = 1, size = study_data$sampleSize[k], prob = p_ik))
-  #   }
-  #   }
-  # }
-  # 
-  # NS = 24
-  # NT = 4
-  # N = nrow(sim_dat)
-  # s = sim_dat$study
-  # # t = sim_dat$treatment
-  # t = as.integer(factor(sim_dat$treatment, levels = c("A", "B", "C", "D"), labels = c(1, 2, 3, 4)))
-  # y = sim_dat$responders
-  # n = sim_dat$sampleSize
-  # drug_list<-unique(sim_dat$treatment)
-  # drug_list<- c("A","B","C","D")
-  # Narm <- as.numeric(table(sim_dat$study))
-  # n.obs <- matrix(NA,nrow=NS, ncol=max(Narm))
-  # n.eve <- matrix(NA,nrow=NS, ncol=max(Narm))
-  # dr <- matrix(NA,nrow=NS, ncol=max(Narm))
-  # study<-unique(sim_dat$study) 
-  # 
-  # data_CB <- list('Narm'=N, 'Nstudy'=NS, 
-  #                 'Ndrug'=NT, 'study'= s, 'drug'=t, 
-  #                 'y'=y, 'n'=n ,'Omega'=diag(rep(0.2,times=4)))
-  # inits_CB <- list(list(mu=rep(0,max(NS)), d=c(NA,rep(1,max(t)-1))),
-  #                  list(mu=rep(0,max(NS)), d=c(NA,rep(0,max(t)-1))))
-  # 
-  # para_CB<-c( "d", "tau", "best1", 'best2', 'best3')
-  # fit_CB<-jags(data=data_CB, inits=inits_CB, para_CB,
-  #              n.iter=5000, n.burnin = 2000, n.chains = 2, n.thin = 1,
-  #              DIC=TRUE, model.file=CBWish)
-  # 
-  # #saving treatment effect output
-  # CB_trt_results<-data.frame(fit_CB$BUGSoutput$summary[,c(1, 3, 7)])
-  # CB_trt_results <- tibble::rownames_to_column(CB_trt_results, "drug_list")
-  # CB_trt_results<-CB_trt_results%>%
-  #   filter(drug_list %in% c("d[1]", "d[2]", "d[3]", "d[4]"))
-  # 
-  # CBresults <- CB_trt_results%>%
-  #   mutate(LL = as.numeric(X2.5.),
-  #          UL = as.numeric(X97.5.),
-  #          mean = as.numeric(mean))
-  # 
-  # AB_lower_95 = CBresults[2,3]
-  # AB_upper_95 = CBresults[2,4]
-  # AB_reject = 1-as.numeric(0 >= AB_lower_95 & 0 <= AB_upper_95)
-  # 
-  # AC_lower_95 = CBresults[3,3]
-  # AC_upper_95 = CBresults[3,4]
-  # AC_reject = 1-as.numeric(0 >= AC_lower_95 & 0 <= AC_upper_95)
-  # 
-  # AD_lower_95 = CBresults[4,3]
-  # AD_upper_95 = CBresults[4,4]
-  # AD_reject = 1-as.numeric(0 >= AD_lower_95 & 0 <= AD_upper_95)
-  # 
-  # reject_correct_CB = c(AB_reject, AC_reject, AD_reject) 
-  
+
   # return power results
   return(rbind(reject_correct_LA, reject_correct_AB))
 }
