7d08443ee1ba355689e857402087930a48264582
Yicheng Shen <shenyicheng1109@gmail.com>
2024-02-07 10:39:59 -0500

    update

--- Arm-based Simulation New/AB_simulation.R
+++ Arm-based Simulation New/AB_simulation.R
@@ -8,7 +8,7 @@ library(gemtc)
 set.seed(123456)
 
 N_cores = detectCores() 
-N_sim = 400
+N_sim = 800
 
 
 load("df_indirect_blank.RData")
@@ -16,6 +16,11 @@ load("df_direct_blank.RData")
 load("df_BNMA_blank.RData")
 load("df_indirect_bias_additional_blank.RData")
 
+
+df_BNMA_blank = df_BNMA_blank[,1:10]
+df_direct_blank = df_direct_blank[,1:8]
+df_indirect_blank = df_indirect_blank[,1:9]
+df_indirect_bias_additional_blank = df_indirect_bias_additional_blank[,1:9]
 
 source("power_sim_AB.R")
 
